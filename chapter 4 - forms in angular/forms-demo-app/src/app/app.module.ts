fe310e099e06ae44494e5ca55870bb000f589b02
Ross Moug <ross.moug@gmail.com>
2018-07-17 21:31:57 +0100

    Forms using formGroup and formControl

--- chapter 4 - forms in angular/forms-demo-app/src/app/app.module.ts
+++ chapter 4 - forms in angular/forms-demo-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoFormSkuComponent
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent
   ],
   imports: [
     BrowserModule,
